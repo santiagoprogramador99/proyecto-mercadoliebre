e09fdab0c2b3586f573fdf907b0cdc8ae6d7a35d
santiagoprogramador99 <santi.maestro99@gmail.com>
2023-05-24 21:18:12 -0500

    cambios scripts package json

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 app.use(express.static("public"))
 
-app.listen(process.env.PORT || 8000, () => console.log("servidor funcionando"))
+app.listen(process.env.PORT || 3000, () => console.log("servidor funcionando en el puerto 3000"))
 
 
 app.get("/", (req, resp)=>{
